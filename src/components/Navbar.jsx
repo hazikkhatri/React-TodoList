e100605c18a54e97d9f4b6207b36b76813ec6059
Hazik Khatri <142565556+hazikkhatri@users.noreply.github.com>
2024-08-19 15:36:46 +0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Navbar = () => {
     return (
         <nav className='flex py-3 bg-violet-600 justify-around items-center'>
-            <div className="text-white font-bold text-2xl">iTask</div>
-            <ul className='flex gap-4 items-center'>
-                <li className='text-white text-base'>Home</li>
-                <li className='text-white text-base'>Your Task</li>
+            <div className="text-white font-bold text-3xl cursor-pointer">iTask</div>
+            <ul className='flex gap-8 items-center'>
+                <li className='text-white text-base cursor-pointer'>Home</li>
+                <li className='text-white text-base cursor-pointer'>Your Task</li>
             </ul>
         </nav>
     )
