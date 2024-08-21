5c332ae03c69f1ee5cf595c3fa112f88ff0cb08d
Hazik Khatri <142565556+hazikkhatri@users.noreply.github.com>
2024-08-21 08:59:40 +0500

    title changed

--- src/App.jsx
+++ src/App.jsx
@@ -57,11 +57,11 @@ function App() {
       <Navbar />
       <div className="container mx-auto bg-violet-200 mt-4 rounded-md p-3 min-h-[80vh]">
         <div className="AddTodo">
-          <h2 className='text-xl font-medium'>Add a Todo</h2>
+          <h2 className='text-2xl font-medium'>Add a Todo</h2>
           <input onChange={handleChange} value={todo} type="text" className='w-80 px-2 py-1 my-3 text-lg focus:outline-none' />
           <button onClick={handleAdd} className='bg-violet-500 text-white px-2 py-1 rounded ml-2'>Save</button>
         </div>
-        <h2 className='text-xl mb-3 font-medium'>Your Todos</h2>
+        <h2 className='text-xl mb-3 font-medium'>Your Todos:</h2>
         <div className="todos">
           {todos.length === 0 && <div className='font-medium text-xl mt-4'>No todos to display</div>}
           {todos.map(item => {
